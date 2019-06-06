93dc963f6e218517752c40ed84d571ed9cced44b
Max Alva <alva85@gmail.com>
2019-06-06 01:30:11 -0500

    home page

--- lib/src/pages/home_temp.dart
+++ lib/src/pages/home_temp.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
 
 class HomePageTemp extends StatelessWidget {
+  final items = [
+    'Item Uno',
+    'Item Dos',
+    'Item Tres',
+    'Item Cuatro',
+    'Item Cinco'
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Temp'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Componentes Temp'),
+      ),
+      body: ListView(children: _crearItems2()),
     );
+  }
+
+  List<Widget> _crearItems() {
+    List<Widget> itemsList = new List<Widget>();
+
+    for (String item in items) {
+      final tempWidget = ListTile(title: Text(item));
+      itemsList..add(tempWidget)..add(Divider());
+    }
+
+    return itemsList;
+  }
+
+  List<Widget> _crearItems2() {
+    var widgets = items.map((item) {
+      return Column(
+        children: <Widget>[
+          ListTile(
+            title: Text(item),
+            subtitle: Text('Subtítulo del item'),
+            leading: Icon(Icons.accessibility),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: () {},
+          ),
+          Divider(),
+        ],
+      );
+    }).toList();
+
+    return widgets;
   }
 }
