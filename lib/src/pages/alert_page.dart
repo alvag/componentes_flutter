fe29ab6ebecc3e5c42e21ceafc80b8b2a6b87696
Max Alva <alva85@gmail.com>
2019-06-06 23:55:55 -0500

    Widget Alert

--- lib/src/pages/alert_page.dart
+++ lib/src/pages/alert_page.dart
@@ -7,6 +7,15 @@ class AlertPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alert Page'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Mostrar Alerta'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => _mostrarAlerta(context),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.arrow_back),
         onPressed: () {
@@ -14,5 +23,44 @@ class AlertPage extends StatelessWidget {
         },
       ),
     );
+  }
+
+  void _mostrarAlerta(BuildContext context) {
+    final _borderAlert =
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(20));
+    final _borderButton =
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(20));
+    showDialog(
+        context: context,
+        // barrierDismissible: true,
+        builder: (context) {
+          return AlertDialog(
+            shape: _borderAlert,
+            title: Text('Título del Alert!'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text('Contenido de la caja del Alert!!'),
+                FlutterLogo(size: 100.0)
+              ],
+            ),
+            actions: <Widget>[
+              FlatButton(
+                // shape: _borderButton,
+                // color: Colors.lime,
+                child: Text('Cancelar'),
+                onPressed: () => Navigator.of(context).pop(),
+              ),
+              FlatButton(
+                // shape: _borderButton,
+                // color: Colors.lime,
+                child: Text('OK'),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
   }
 }
