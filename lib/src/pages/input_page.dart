e39e39b007259aacad2f3bdc207b4dc068047d77
Max Alva <alva85@gmail.com>
2019-06-07 01:16:37 -0500

    Inputs

---
+++ lib/src/pages/input_page.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  String _nombre = '';
+  String _email = '';
+  String _password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inputs'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20),
+        children: <Widget>[
+          _crearInput(),
+          Divider(),
+          _crearEmail(),
+          Divider(),
+          _crearPassword(),
+          Divider(),
+          _crearPersona(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.arrow_back),
+        onPressed: () => Navigator.pop(context),
+      ),
+    );
+  }
+
+  Widget _crearInput() {
+    return TextField(
+      autofocus: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        counter: Text('Letras ${_nombre.length}'),
+        hintText: 'Nombre y Apellidos',
+        labelText: 'Nombre',
+        helperText: 'Sólo se permiten letras',
+        suffixIcon: Icon(Icons.accessibility),
+        icon: Icon(Icons.account_circle),
+      ),
+      onChanged: (value) {
+        setState(() {
+          _nombre = value;
+        });
+      },
+    );
+  }
+
+  Widget _crearEmail() {
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        counter: Text('Letras ${_email.length}'),
+        hintText: 'Correo Electrónico',
+        labelText: 'Email',
+        helperText: 'Ingresa tu correo',
+        suffixIcon: Icon(Icons.email),
+        icon: Icon(Icons.alternate_email),
+      ),
+      onChanged: (value) {
+        setState(() {
+          _email = value;
+        });
+      },
+    );
+  }
+
+  Widget _crearPassword() {
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+        counter: Text('Letras ${_password.length}'),
+        hintText: 'Contraseña',
+        labelText: 'Contraseña',
+        helperText: 'Ingresa tu contraseña',
+        suffixIcon: Icon(Icons.lock_open),
+        icon: Icon(Icons.lock),
+      ),
+      onChanged: (value) {
+        setState(() {
+          _email = value;
+        });
+      },
+    );
+  }
+
+  Widget _crearPersona() {
+    return ListTile(
+      title: Text('Nombre es: $_nombre'),
+      subtitle: Text('Email: $_email'),
+    );
+  }
+}
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'package:componentes_flutter/src/pages/input_page.dart';
 import 'package:componentes_flutter/src/pages/animated_container.dart';
 import 'package:componentes_flutter/src/pages/card_page.dart';
 import 'package:componentes_flutter/src/pages/alert_page.dart';
@@ -12,6 +13,7 @@ Map<String, WidgetBuilder> getAppRoutes() {
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
     'animatedComtainer': (context) => AnimatedContainerPage(),
-    'card': (context) => CardPage()
+    'card': (context) => CardPage(),
+    'inputs': (context) => InputPage()
   };
 }
